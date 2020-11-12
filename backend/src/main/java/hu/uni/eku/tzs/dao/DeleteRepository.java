c76dc2b0eddeb7a005a30c8b69fc208bcbac1e84
NagyAkos <darkmyth1997@gmail.com>
2020-11-12 23:18:17 +0100

    Delete eltávolítása
    Felesleges delete funkció eltávolítása

--- backend/src/main/java/hu/uni/eku/tzs/dao/DeleteRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.uni.eku.tzs.dao;
-
-import hu.uni.eku.tzs.dao.entity.Delete;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DeleteRepository extends CrudRepository<Delete, Integer> {
-}
