71c6e774ffbcbea9102d7e9c28b51abadace07c3
Karaba Márk <karaba.matyika@gmail.com>
2020-11-17 19:23:53 +0100

    Reserve kibővítése
    Reserve kibővítése
    
    ref: #21

--- frontend/src/component/ReservationList.js
+++ frontend/src/component/ReservationList.js
@@ -24,5 +24,48 @@ class ReservationList extends React.Component {
     _updateState() {
         this.setState({reservations: store._guests});
     }
+
+
+    render() {
+        return (
+            <div>
+                <button className="btn btn-info" onClick={() => actions.fetchReserves(this.state)}>Listáz</button>
+                <div div className="table-responsive">
+                    <table className="product_table table">
+                        <thead>
+                        <tr>
+                            <td>Hely ID</td>
+                            <td>Típus</td>
+                            <td>Áram</td>
+                            <td>Vendég ID</td>
+                            <td>ID</td>
+                            <td>Ár</td>
+                            <td>Foglalás kezdete</td>
+                            <td>Foglalás vége</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.reservations.map(({campId, campingStyle, electricity, guestId, id, price, reserveEnd, reserveStart}) => {
+                            return (
+                                <tr key={id}>
+                                    <td>{campId}</td>
+                                    <td>{campingStyle}</td>
+                                    <td>{electricity}</td>
+                                    <td>{guestId}</td>
+                                    <td>{price}</td>
+                                    <td>{reserveEnd}</td>
+                                    <td>{reserveStart}</td>
+
+
+                                </tr>
+                            );
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        );
+    }
 }
 export default ReservationList
